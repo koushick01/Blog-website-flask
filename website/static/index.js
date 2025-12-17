ee7a4c1a56012f26b756d624ac4b839c010b016d
koushick01 <koushicks01@gmail.com>
2025-12-17 06:34:15 -0500

    first commit

---
+++ website/static/index.js
@@ -0,0 +1,15 @@
+function like(postId){
+    likeCount = document.getElementById(`like-count-${postId}`);
+    likeButton = document.getElementById(`like-button-${postId}`);
+
+    fetch(`/like-post/${postId}`, { method: "POST" }).then((res) => res.json()).then((data) => {
+        likeCount.innerHTML = data["likes"];
+        if (data["liked"] === true){
+            likeButton.className="fa-solid fa-thumbs-up";
+        }
+        else{
+            likeButton.className="fa-regular fa-thumbs-up";
+        }
+    })
+    .catch((e) => (alert("Could not like the post")))
+}
